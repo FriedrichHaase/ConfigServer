5336a5e67673094b1e32efbcaf8b12ba12314cc7
FriedrichHaase <122970991+FriedrichHaase@users.noreply.github.com>
2023-02-22 10:58:25 +0100

    Add files via upload

---
+++ Controllers/UserRegistrationController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using System.Collections;
+using System.Net.Http;
+
+namespace TestConfigServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserRegistrationController : Controller
+    {
+        private readonly ApplicationUser _auc;
+
+        public UserRegistrationController(ApplicationUser auc)
+        {
+            _auc = auc;
+            
+        }
+
+        private void gehtEndlich(User user)
+        {
+            _auc.Add(user);
+            _auc.SaveChanges();
+        }
+        /*
+        public IActionResult Create()
+        {
+            return View();
+        }
+        */
+
+       /* private static void AddStudent()
+        {
+
+            using (var application = new ApplicationUser())
+            {
+
+                var user = new User
+                {
+                    EMail = "muster@mail.to",
+                    Password = "qwertz",
+                    ConfPassword = "qwertz",
+                    Firstname = "Max",
+                    Surname = "Mustermann"
+                };
+
+                application.UserRegistration.Add(user);
+                application.SaveChanges();
+
+            }
+        } */
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+         public IActionResult Create(User uc)
+         {
+            // User user = new User();
+            
+            _auc.Add(uc);
+            _auc.SaveChanges();
+             ViewBag.message = "The user "+ uc.Firstname + " " + uc.Surname + " was created successfully!";
+
+             //AddStudent();
+             return View();
+         }
+        
+
+       /* [HttpPost]
+        [ValidateAntiForgeryToken]
+        public string PostDataToDB(/*User uc)
+        {
+           // User user = new User(uc.EMail, uc.Password, uc.ConfPassword, uc.Firstname, uc.Surname);
+           // ViewBag.message = "The user " + uc.Firstname + " " + uc.Surname + " was created successfully!";
+            //validate and write to database
+            return "Hallo";
+        }*/
+    }
+}
---
+++ User.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestConfigServer
+{
+    public class User
+    {
+
+        [Key]
+        [Required]
+        public string UserID { get => NewUserID(); set => NewUserID(); }
+
+        [Required(ErrorMessage = "Please enter your e-mail adress")]
+        [Display(Name = "EMail")]
+        public string? EMail { get; set; }
+
+        [Required(ErrorMessage ="Please enter a password")]
+        [DataType(DataType.Password)]   
+        [Display(Name = "Password")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string? ConfPassword { get; set; }
+
+        [Required(ErrorMessage = "Please enter your firstname")]
+        [Display(Name = "Firstname")]
+        public string? Firstname { get; set; }
+
+        [Required(ErrorMessage ="Please enter your surname")]
+        [Display(Name = "Surname")]
+        public string? Surname { get; set; }
+
+        [Required]
+        public string? CreationDate { get => CurrentDate(); set => CurrentDate(); }
+
+        public User(string eM, string ps, string cps, string fn, string sn)
+        {
+            NewUserID();
+            CurrentDate();
+            EMail = eM;
+            Password = ps;
+            ConfPassword = cps;
+            Firstname = fn;
+            Surname = sn;
+        }
+
+        public User()
+        {
+            NewUserID();
+            CurrentDate();
+        }
+
+        private string NewUserID()
+        {
+            Guid UserID = Guid.NewGuid();
+            return UserID.ToString();
+        }
+
+        private static string CurrentDate()
+        {
+            string today = DateTime.UtcNow.ToShortDateString();
+
+            return today;
+        }
+    }
+}
